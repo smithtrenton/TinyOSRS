01d407483177f361ade573e217351eef9817a441
Trenton Smith <tlsdude@gmail.com>
2020-03-20 20:42:06 -0600

    remove unused imports

--- src/org/tls/Main.java
+++ src/org/tls/Main.java
@@ -1,8 +1,5 @@
 package org.tls;
 
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
-import java.applet.Applet;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.util.Optional;
@@ -10,19 +7,19 @@ import java.util.Optional;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-	    System.setProperty("sun.awt.noerasebackground", "true");
+        System.setProperty("sun.awt.noerasebackground", "true");
 
-		String username = System.getProperty("java.net.socks.username");
-		if (username != null) {
-			Optional<String> password = Optional.ofNullable(System.getProperty("java.net.socks.password"));
-			Authenticator.setDefault(new Authenticator() {
-				@Override
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return (new PasswordAuthentication(username, password.orElse("").toCharArray()));
-				}
-			});
-		}
+        String username = System.getProperty("java.net.socks.username");
+        if (username != null) {
+            Optional<String> password = Optional.ofNullable(System.getProperty("java.net.socks.password"));
+            Authenticator.setDefault(new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return (new PasswordAuthentication(username, password.orElse("").toCharArray()));
+                }
+            });
+        }
 
-		RSFrame.start();
+        RSFrame.start();
     }
 }
